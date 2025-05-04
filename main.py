a1eb98c8d4170547698b2ee205bf34c10d1b772f
Harshveer Singh Raka <harshveerraka@gmail.com>
2025-05-04 15:40:41 +0530

    Creating Frames (PNGs with pixels)
    bytes to bit conversion
    creating frames

--- main.py
+++ main.py
@@ -4,12 +4,33 @@ import numpy as np
 
 
 def read_The_File_BROOO(filePath):
+    # opens file in 'rb' mode means reading file in raw binary
     with open(filePath, 'rb') as f:
-        return f.read()
+        return f.read() # after reading byte-by-byte returning all bytes
+
+def bytesToBits(byte):
+    return[(byte >> i) & 1 for i in reversed(range(8))] # converting bytes to bits
+
+# Encryption function will be added here
+
+def CreatingFrames(data,width,height,output_frames):
+    bits = [] # list of bits
+    for byte in data:
+        bits.extend(bytesToBits(byte)) # for every byte converted append bits into bits list
+    bitsPerFrame = height * width
+    numOfFrames = (len(bits) + bitsPerFrame -1) // bitsPerFrame
+
+    os.makedirs(output_frames, exist_ok= True) # dir to store all PNGs ('exit_ok' so that it don't crash with existing folder)
+    bit_index = 0
+
+
+    # exhausted sleeping ...ZZZZZZ..ZZZZ
+
 
 
 def main():
     filePath = input("Enter the file path: ")
+    width = input("Enter the width ()")
     # checks if the file exists or nawt
     if(os.path.exists(filePath)):
         print("Path -> " + filePath)
